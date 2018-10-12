4cb595ec89528450e9b068cb20b5cab53ff6f4dd
Jack <jacka7901@gmail.com>
2018-10-11 21:08:42 -0400

    TwoSum problem.

--- TwoSum.py
+++ TwoSum.py
@@ -1,14 +1,11 @@
 
-class Solution():
-    def twoSum( nums, target):
+class Solution(object):
+    def twoSum(self, nums, target):
 
         for i in range(0, len(nums) - 1, 1):
             for j in range(i + 1, len(nums), 1):
                 if nums[i] + nums[j] == target:
-                    print([i,j])
+                    return([i,j])
 
 
 
-test = [2, 7, 11, 15]
-targetnum = 22
-Solution.twoSum( test, targetnum)
